b6dda85688fec1fcbf787c14b02b6689e02d5238
bakudas <bakudas@gmail.com>
2018-11-16 01:40:12 -0200

    add holes, embed and help stuff

---
+++ commands/dandy.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'dandy',
+    description: 'Mostra um troço bonitinho',
+    execute(message, args) {
+        // inside a command, event listener, etc.
+        const exampleEmbed = new Discord.RichEmbed()
+        .setColor('#0099ff')
+        .setTitle('Dandy and Randy')
+        .setURL('https://store.steampowered.com/app/959130/Dandy__Randy/')
+        .setAuthor('Yin', 'https://images-ext-1.discordapp.net/external/mcJYRwBpJ-4xFFSjIrBACyJxGkpLDf-J0iNIvzaw5To/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/143454766121353216/bfe54e2db970ef3061b47f457980dc73.png', 'https://twitter.com/AndreYin')
+        .setDescription('Dandy and Randy is a 2D top-down old-school arcade game featuring two of the worst archaeologists to ever grace the Earth. Help them in their adventures by finding treasure defeating enemies, fighting bosses and discovering secrets! Can they pay off their debts? It\'s up to you to find out!')
+        .setThumbnail('https://pbs.twimg.com/profile_images/1048408177853812736/1VMCZay__400x400.jpg')
+        .addBlankField()
+        .addField('STEAM', 'https://store.steampowered.com/app/959130/Dandy__Randy/', true)
+        .setImage('https://media.discordapp.net/attachments/496743144054980608/504345975834542111/unknown.png')
+        .setTimestamp()
+        .setFooter('Um jogo feito pelo Yin', 'https://images-ext-1.discordapp.net/external/mcJYRwBpJ-4xFFSjIrBACyJxGkpLDf-J0iNIvzaw5To/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/143454766121353216/bfe54e2db970ef3061b47f457980dc73.png');
+
+        message.channel.send(exampleEmbed);
+    },
+};
+
+let randomMsg = 0; // var para 'randomizar' as mensagens da vaca
---
+++ commands/help.js
@@ -0,0 +1,46 @@
+const { prefix } = require('../config.json');
+
+module.exports = {
+    name: 'help',
+    description: 'List all of my commands or info about a specific command.',
+    aliases: ['commands'],
+    usage: '[command name]',
+    cooldown: 5,
+    execute(message, args) {
+        const data = [];
+        const { commands } = message.client;
+
+        if (!args.length) {
+            data.push('Segue a lista de todos os meus comandos:');
+            data.push(commands.map(command => command.name).join(', '));
+            data.push(`\nVocê pode usar \`${prefix}help [nome do comando]\` para ter mais infos sobre o comendo!`);
+
+            return message.author.send(data, { split: true })
+                .then(() => {
+                    if (message.channel.type === 'dm') return;
+                    message.reply('Mandei uma DM com todos os meus comandos disponíveis! (: ');
+                })
+                .catch(error => {
+                    console.error(`Não foi possível mandar DM para ${message.author.tag}.\n`, error);
+                    message.reply('Parace que suas DMs estão desativadas. Não consigo de mandar mensagem. :/ ');
+                });
+        }
+
+        const name = args[0].toLowerCase();
+        const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+        if (!command) {
+            return message.reply('Não é um comando válido');
+        }
+
+        data.push(`**Nome:** ${command.name}`);
+
+        if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+        if (command.description) data.push(`**Descrição:** ${command.description}`);
+        if (command.usage) data.push(`**Uso:** ${prefix}${command.name} ${command.usage}`);
+
+        data.push(`**Tempo de espera:** ${command.cooldown || 3} second(s)`);
+
+        message.channel.send(data, { split: true });
+    },
+};
